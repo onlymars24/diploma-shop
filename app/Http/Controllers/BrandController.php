487a9736ad5a02b5198c59edbe22d3d6d0935988
onlymars24 <marsel.galimov.241@gmail.com>
2024-06-01 04:29:21 +0300

    c2

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -38,9 +38,9 @@ class BrandController extends Controller
         Log::info(json_encode($request->all()));
         Log::info('$request->brandId ');
         Log::info('$request->brandId '.$request->brandId);
-        if($request->hasFile('file')){
-            ImageService::upload('brands', $request->file('file'), $request->brandId);
-        }
+        // if($request->hasFile('file')){
+        //     ImageService::upload('brands', $request->file('file'), $request->brandId);
+        // }
     
     }
 }
