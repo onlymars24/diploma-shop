6335aed5478be58f2fa9e6e9e94cd2dd354088d3
onlymars24 <marsel.galimov.241@gmail.com>
2024-06-01 04:01:28 +0300

    c2

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -35,6 +35,7 @@ class BrandController extends Controller
     }
 
     public function uploadImage(Request $request){
+        Log::info('$request->brandId ');
         Log::info('$request->brandId '.$request->brandId);
         if($request->hasFile('file')){
             ImageService::upload('brands', $request->file('file'), $request->brandId);
