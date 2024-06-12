2a1753123b78325eeebb401a52b0e84078b9bd80
onlymars24 <marsel.galimov.241@gmail.com>
2024-06-12 23:20:47 +0300

    c2

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -25,7 +25,7 @@ class OrderController extends Controller
             $detail = Detail::with(['type', 'design', 'brand', 'modification', 'generation'])->find($el['id']);
             $list[] = ['detail' => $detail->toArray(), 'quantity' => $el['quantity']];
             $total += $detail->price;
-            $detail->count = $detail->count-1;
+            $detail->count = $detail->count-$el['quantity'];
             $detail->save();
         }
         $user = Auth::user();
